e70f2ecbb6b244d53f15e1f0749504b04fc1b4f9
Matt Salmons <mattcsalmons@gmail.com>
2021-10-23 15:05:59 -0600

    writes create subscriber route

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -18,8 +18,17 @@ router.get('/:id', (req, res) => {
   res.send(`get subscriber ${id}`);
 });
 // Create a subscriber
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 // Update a subscriber
 router.patch('/:id', (req, res) => {
